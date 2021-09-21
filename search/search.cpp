6f97d350861fab7d42062aad940b9d52affd3c49
Pogosov Arseny <pogosovarseny@gmail.com>
2021-09-21 11:30:27 +0300

    search

--- search/search.cpp
+++ search/search.cpp
@@ -18,8 +18,9 @@ public:
 struct StringViewCompCaseIndep {
 public:
     bool operator()(const std::string_view& str1, const std::string_view& str2) const {
-        if (str1.length() != str2.length())
+        if (str1.length() != str2.length()) {
             return false;
+        }
         return std::equal(str1.begin(), str1.end(), str2.begin(),
                           [](auto a, auto b) { return std::tolower(a) == std::tolower(b); });
     }
@@ -32,7 +33,7 @@ void ReadWord(std::string_view::const_iterator begin, std::string_view::const_it
     }
     for (word_end = word_begin; word_end != end && std::isalpha(*word_end); ++word_end) {
     }
-    word = std::string_view(&*word_begin, std::max(0, word_end - word_begin));
+    word = std::string_view(&*word_begin, std::max(0, static_cast<int>(word_end - word_begin)));
 }
 
 std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
@@ -52,7 +53,7 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         for (std::string_view::iterator word_begin = line.begin(); word_begin != line.end();) {
             std::string_view to_add;
             ReadWord(word_begin, line.end(), to_add, word_begin);
-            if (to_add.size()) {
+            if (!to_add.empty()) {
                 ++text_parsed.back()[to_add];
                 ++text_block_size.back();
             }
