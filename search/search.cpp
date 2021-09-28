304d6bef9b762329219f9f15b230f049e8334c1d
Pogosov Arseny <pogosovarseny@gmail.com>
2021-09-28 14:28:17 +0300

    search

--- search/search.cpp
+++ search/search.cpp
@@ -1,21 +1,30 @@
+#include "search.h"
+
 #include <cctype>
 #include <cmath>
 #include <unordered_set>
 #include <unordered_map>
 #include <numeric>
 #include <algorithm>
-#include "search.h"
+#include <functional>
 
-struct StringViewHashCaseIndep {
+struct StringViewHashCaseIndependent {
+private:
+    const uint_fast64_t _p = 31;
+    const uint_fast64_t _m = 1e9 + 7;
+
 public:
     size_t operator()(const std::string_view& str) const {
-        std::string s(str.size(), 'a');
-        std::transform(str.begin(), str.end(), s.begin(), [](char a) { return std::tolower(a); });
-        return std::hash<std::string>()(s);
+        uint_fast64_t result = 0;
+        for (char i : str) {
+            result = (result * _p + tolower(i) - 'a' + 1) % _m;
+        }
+
+        return static_cast<size_t>(result);
     }
 };
 
-struct StringViewCompCaseIndep {
+struct StringViewCompCaseIndependent {
 public:
     bool operator()(const std::string_view& str1, const std::string_view& str2) const {
         if (str1.length() != str2.length()) {
@@ -26,49 +35,65 @@ public:
     }
 };
 
-void ReadWord(std::string_view::const_iterator begin, std::string_view::const_iterator end, std::string_view& word,
-              std::string_view::iterator& word_end) {
-    std::string_view::iterator word_begin = begin;
-    for (; word_begin != end && !std::isalpha(*word_begin); ++word_begin) {
+std::string_view ReadBlock(std::string_view& text, std::function<bool(char)> is_block_part) {
+    while (!text.empty() && !is_block_part(text.front())) {
+        text.remove_prefix(1);
     }
-    for (word_end = word_begin; word_end != end && std::isalpha(*word_end); ++word_end) {
+    size_t end;
+    for (end = 0; end < text.size() && is_block_part(text[end]); ++end) {
     }
-    word = std::string_view(&*word_begin, std::max(0, static_cast<int>(word_end - word_begin)));
+
+    std::string_view result = text.substr(0, end - 0);
+    text.remove_prefix(end - 0);
+
+    return result;
 }
 
-std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
-    std::vector<std::string_view> text_block;
-    std::vector<std::unordered_map<std::string_view, int, StringViewHashCaseIndep, StringViewCompCaseIndep>>
-        text_parsed;
-    std::vector<int> text_block_size;
-    for (size_t line_begin = 0; line_begin < text.size(); ++line_begin) {
-        size_t line_end = std::min(text.size(), text.find('\n', line_begin));
-        std::string_view line = text.substr(line_begin, line_end - line_begin);
-        line_begin = line_end;
+using ParsedTextStructure = std::vector<
+    std::unordered_map<std::string_view, int, StringViewHashCaseIndependent, StringViewCompCaseIndependent>>;
+using ParsedQueryStructure =
+    std::unordered_set<std::string_view, StringViewHashCaseIndependent, StringViewCompCaseIndependent>;
 
-        text_block.push_back(line);
-        text_parsed.push_back({});
+void ParseText(std::string_view text, std::vector<std::string_view>& text_block, std::vector<int>& text_block_size,
+               ParsedTextStructure& text_parsed) {
+    text_block.clear();
+    text_block_size.clear();
+    text_parsed.clear();
+
+    while (!text.empty()) {
+        std::string_view block = ReadBlock(text, [](char ch) { return ch != '\n'; });
+        if (block.empty()) {
+            break;
+        }
+
+        text_block.push_back(block);
         text_block_size.push_back(0);
-        for (std::string_view::iterator word_begin = line.begin(); word_begin != line.end();) {
-            std::string_view to_add;
-            ReadWord(word_begin, line.end(), to_add, word_begin);
-            if (!to_add.empty()) {
-                ++text_parsed.back()[to_add];
-                ++text_block_size.back();
+        text_parsed.push_back({});
+        while (!block.empty()) {
+            std::string_view word = ReadBlock(block, isalpha);
+            if (block.empty()) {
+                break;
             }
+            ++text_parsed.back()[word];
+            ++text_block_size.back();
         }
     }
+}
 
-    std::unordered_set<std::string_view, StringViewHashCaseIndep, StringViewCompCaseIndep> query_parsed;
-    for (auto word_begin = query.begin(); word_begin != query.end();) {
-        std::string_view to_add;
-        ReadWord(word_begin, query.end(), to_add, word_begin);
-        if (!to_add.empty()) {
-            query_parsed.insert(to_add);
+void ParseQuery(std::string_view query, ParsedQueryStructure& query_parsed) {
+    while (!query.empty()) {
+        std::string_view word;
+        word = ReadBlock(query, isalpha);
+        if (word.empty()) {
+            break;
         }
+        query_parsed.insert(word);
     }
+}
 
-    int n = text_parsed.size();
+std::vector<double> GetScore(const std::vector<int>& text_block_size, ParsedTextStructure text_parsed,
+                             ParsedQueryStructure query_parsed) {
+    size_t n = text_parsed.size();
     std::vector<double> score(n);
     for (auto& word : query_parsed) {
         int count = 0;
@@ -78,22 +103,37 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         if (count == 0) {
             continue;
         }
-        double idf = log(n / count);
-        for (size_t i = 0; i < static_cast<size_t>(n); ++i) {
+        double idf = log(static_cast<double>(n) / count);
+        for (size_t i = 0; i < n; ++i) {
             score[i] += static_cast<double>(text_parsed[i][word]) / text_block_size[i] * idf;
         }
     }
 
-    std::vector<int> ord(n);
-    std::iota(ord.begin(), ord.end(), 0);
-    std::sort(ord.begin(), ord.end(), [&](int a, int b) { return score[a] > score[b]; });
+    return score;
+}
 
-    std::vector<std::string_view> res;
-    for (size_t i = 0; i < results_count && i < static_cast<size_t>(n); ++i) {
-        if (score[ord[i]] > 0) {
-            res.push_back(text_block[ord[i]]);
+std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
+    std::vector<std::string_view> text_block;
+    std::vector<int> text_block_size;
+    ParsedTextStructure text_parsed;
+    ParseText(text, text_block, text_block_size, text_parsed);
+
+    ParsedQueryStructure query_parsed;
+    ParseQuery(query, query_parsed);
+
+    std::vector<double> score = GetScore(text_block_size, text_parsed, query_parsed);
+
+    std::vector<size_t> ordered_score_indexes(score.size());
+    std::iota(ordered_score_indexes.begin(), ordered_score_indexes.end(), 0);
+    std::sort(ordered_score_indexes.begin(), ordered_score_indexes.end(),
+              [&](size_t a, size_t b) { return score[a] > score[b]; });
+
+    std::vector<std::string_view> result;
+    for (size_t i = 0; i < results_count && i < score.size(); ++i) {
+        if (score[ordered_score_indexes[i]] > 0) {
+            result.push_back(text_block[ordered_score_indexes[i]]);
         }
     }
 
-    return res;
+    return result;
 }
