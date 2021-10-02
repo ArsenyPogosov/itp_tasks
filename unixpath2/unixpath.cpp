972769c7dadb217b126d00c9428d5b6673e3f35a
Pogosov Arseny <pogosovarseny@gmail.com>
2021-10-02 19:22:02 +0300

    unixpath2

---
+++ unixpath2/unixpath.cpp
@@ -0,0 +1,101 @@
+#include "unixpath.h"
+
+#include <algorithm>
+
+UnixPath::ParsedPath::ParsedPath() {
+    content_ = {""};
+}
+
+UnixPath::ParsedPath::ParsedPath(std::string_view path) {
+    ParseFrom(path);
+}
+
+void UnixPath::ParsedPath::ParseFrom(std::string_view path) {
+    content_.clear();
+    for (auto block_begin = path.begin(); block_begin < path.end(); ++block_begin) {
+        auto block_end = std::find(block_begin, path.end(), '/');
+        *this += FromBlock(Block(path.substr(block_begin - path.begin(), block_end - block_begin)));
+    }
+}
+
+std::string UnixPath::ParsedPath::Compose() const {
+    std::string result;
+    for (auto& block : content_) {
+        result += static_cast<std::string>(block) + '/';
+    }
+    if (content_.size() > 1) {
+        result.pop_back();
+    }
+
+    return result;
+}
+
+UnixPath::ParsedPath FromBlock(const UnixPath::ParsedPath::Block& block) {
+    UnixPath::ParsedPath::ParsedPath result;
+    result.content_ = {block};
+
+    return result;
+}
+
+UnixPath::ParsedPath RootPath() {
+    return UnixPath::ParsedPath::FromBlock("");
+}
+
+void UnixPath::ParsedPath::operator+=(const ParsedPath& second_part) {
+    for (const auto& block : second_part.content_) {
+        if (block.empty()) {
+            continue;
+        } else if (block == ".") {
+            continue;
+        } else if (block == "..") {
+            if (content_.size() > 1) {
+                content_.pop_back();
+            }
+        } else {
+            content_.push_back(block);
+        }
+    }
+}
+
+bool UnixPath::ParsedPath::IsAbsolute() const {
+    return content_[0].empty();
+}
+
+void UnixPath::ChangeDirectory(std::string_view path) {
+    ParsedPath parsed_path = ParsedPath(path);
+    if (parsed_path.IsAbsolute()) {
+        current_path_ = parsed_path;
+    } else {
+        current_path_ += parsed_path;
+    }
+}
+
+std::string UnixPath::GetAbsolutePath() const {
+    return current_path_.Compose();
+}
+
+std::string UnixPath::GetRelativePath() const {
+    ParsedPath parsed_relative_path = RootPath();
+    size_t n = base_path_.content_.size();
+    size_t m = current_path_.content_.size();
+
+    size_t i1 = 0, i2 = 0;
+    for (; i1 < n && i2 < m; ++i1, ++i2) {
+        if (base_path_.content_[i1] != current_path_.content_[i1]) {
+            break;
+        }
+    }
+    while (i1 < n || i2 < m) {
+        if (i2 >= m) {
+            parsed_relative_path += FromBlock("..");
+            continue;
+        }
+        if (i1 >= n) {
+            parsed_relative_path += FromBlock(current_path_.content_[i2++]);
+            continue;
+        }
+    }
+
+    std::string result = parsed_relative_path.Compose();
+    return (result.empty() ? "." : result);
+}
---
+++ unixpath2/unixpath.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+class UnixPath {
+private:
+    class ParsedPath {
+    public:
+        using Block = std::string;
+
+        std::vector<Block> content_;
+
+        ParsedPath(){};
+        explicit ParsedPath(std::string_view path);
+
+        void ParseFrom(std::string_view path);
+        std::string Compose() const;
+
+        friend ParsedPath FromBlock(const Block& block);
+        friend ParsedPath RootPath();
+
+        void operator+=(const ParsedPath& second_part);
+
+        bool IsAbsolute() const;
+    };
+
+    ParsedPath base_path_;
+    ParsedPath current_path_;
+
+public:
+    UnixPath(std::string_view initial_dir) : base_path_(initial_dir){};
+
+    void ChangeDirectory(std::string_view path);
+
+    std::string GetAbsolutePath() const;
+    std::string GetRelativePath() const;
+};
