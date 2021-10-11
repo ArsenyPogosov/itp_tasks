0dbe2756816d2ca31ee5e901703292e390945d07
Pogosov Arseny <pogosovarseny@gmail.com>
2021-10-11 03:03:34 +0300

    poly

---
+++ poly/poly.cpp
@@ -0,0 +1,162 @@
+#include "poly.h"
+
+#include <algorithm>
+
+Poly::Poly() {
+
+}
+
+Poly::Poly(std::vector<int64_t> coefficients) {
+    for (size_t i = 0; i < coefficients.size(); ++i) {
+        coefficients_[i] = coefficients[i];
+    }
+}
+
+Poly::Poly(std::vector<std::pair<int64_t, int64_t>> coefficients) {
+    for (const auto& [i, ai]: coefficients) {
+        coefficients_[i] = ai;
+    }
+}
+
+int64_t Poly::operator()(int64_t x) const {
+    int64_t result = 0;
+
+    const auto quick_power = [](int64_t x, int64_t k) {
+        int64_t result = 1;
+        for (int64_t i = 0, pi = x; (1 << i) <= k; ++i, pi *= pi) {
+            if ((k >> i) & 1) {
+                result *= pi;
+            }
+        }
+
+        return result;
+    };
+
+    for (const auto& [i, ai]: coefficients_){
+        result += ai * quick_power(x, i);
+    }
+
+    return result;
+}
+
+bool Poly::operator==(const Poly& second) const {
+    return coefficients_ == second.coefficients_;
+}
+
+bool Poly::operator!=(const Poly& second) const {
+    return coefficients_ != second.coefficients_;
+}
+
+Poly Poly::operator+() const {
+    return *this;
+}
+
+Poly Poly::operator-() const {
+    Poly result;
+    result.coefficients_ = coefficients_;
+    for (auto&[i, ai]: result.coefficients_) {
+        ai = -ai;
+    }
+    return result;
+}
+
+Poly Poly::operator+(const Poly& second) const {
+    Poly result;
+    result.coefficients_ = coefficients_;
+    for (const auto& [i, ai]: second.coefficients_) {
+        result.coefficients_[i] += ai;
+        if (!result.coefficients_[i]) {
+            result.coefficients_.erase(i);
+        }
+    }
+
+    return result;
+}
+
+Poly Poly::operator-(const Poly& second) const {
+    Poly result;
+    result.coefficients_ = coefficients_;
+    for (const auto& [i, ai]: second.coefficients_) {
+        result.coefficients_[i] -= ai;
+        if (!result.coefficients_[i]) {
+            result.coefficients_.erase(i);
+        }
+    }
+
+    return result;
+}
+
+Poly& Poly::operator+=(const Poly& second) {
+    for (const auto& [i, ai]: second.coefficients_) {
+        coefficients_[i] += ai;
+        if (!coefficients_[i]) {
+            coefficients_.erase(i);
+        }
+    }
+
+    return *this;
+}
+
+Poly& Poly::operator-=(const Poly& second) {
+    for (const auto& [i, ai]: second.coefficients_) {
+        coefficients_[i] += ai;
+        if (!coefficients_[i]) {
+            coefficients_.erase(i);
+        }
+    }
+
+    return *this;
+}
+
+Poly Poly::operator*(const Poly& second) const {
+    Poly result;
+
+    for (const auto& [i, ai]: coefficients_) {
+        for (const auto& [j, bj] : second.coefficients_) {
+            result.coefficients_[i + j] += ai * bj;
+            if (!result.coefficients_[i]) {
+                result.coefficients_.erase(i);
+            }
+        }
+    }
+
+    return result;
+}
+
+Poly& Poly::operator*=(const Poly& second) const {
+    *this = (*this) * second;
+}
+
+std::ostream& operator<<(std::ostream& out, const Poly& poly){
+    if (poly.coefficients_.empty()) {
+        return out << "0";
+    }
+
+    std::vector<std::pair<int64_t, int64_t>> sorted_coefficients;
+    for (const auto& coefficient: poly.coefficients_) {
+            sorted_coefficients.push_back(coefficient);
+    }
+    std::sort(sorted_coefficients.begin(), sorted_coefficients.end(), std::greater<std::pair<int64_t, int64_t>>());
+
+    for (const auto& [i, ai]: sorted_coefficients) {
+        if (ai > 0) {
+            out << " + ";
+        } else {
+            out << " - ";
+        }
+
+        if (std::abs(ai) != 1 || i == 0) {
+            out << std::abs(ai);
+        }
+
+        if (i != 0) {
+            out << 'x';
+        }
+
+        if (i >= 2) {
+            out << '^' << i;
+        }
+    }
+
+    return out;
+}
---
+++ poly/poly.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+class Poly {
+public:
+    Poly();
+    Poly(std::vector<int64_t> coefficients);
+    Poly(std::vector<std::pair<int64_t, int64_t>> coefficients);
+    Poly(const Poly& from) = default;
+    Poly(Poly&& from) = default;
+
+    Poly& operator=(const Poly& from) = default;
+    Poly& operator=(Poly&& from) = default;
+
+    int64_t operator()(int64_t x) const;
+
+    bool operator==(const Poly& second) const;
+    bool operator!=(const Poly& second) const;
+
+    Poly operator+() const;
+    Poly operator-() const;
+    Poly operator+(const Poly& second) const;
+    Poly operator-(const Poly& second) const;
+
+    Poly& operator+=(const Poly& second);
+    Poly& operator-=(const Poly& second);
+
+    Poly operator*(const Poly& second) const;
+
+    Poly& operator*=(const Poly& second) const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Poly& poly);
+
+private:
+    std::unordered_map<int64_t, int64_t> coefficients_;
+};
