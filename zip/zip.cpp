c3286432a997cd3401e451dad1b2fb5ac14b9946
Pogosov Arseny <pogosovarseny@gmail.com>
2021-10-12 14:33:02 +0300

    zip

--- zip/zip.cpp
+++ zip/zip.cpp
@@ -1,10 +1,49 @@
 #include "zip.h"
 
-Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end) {
-    Zipped result;
-    for (auto i = a_begin, j = b_begin; i != a_end && j != b_end; ++i, ++j) {
-        result.push_back({*i, *j});
-    }
+const ZippedPair Zipped::ConstIterator::operator*() const {
+    return ZippedPair{*first_, *second_};
+}
 
+const ZippedPair Zipped::ConstIterator::operator->() const {
+    return ZippedPair{*first_, *second_};
+}
+
+Zipped::ConstIterator& Zipped::ConstIterator::operator++() {
+    ++first_;
+    ++second_;
+    return *this;
+}
+
+Zipped::ConstIterator Zipped::ConstIterator::operator++(int) {
+    ConstIterator result = *this;
+    ++first_;
+    ++second_;
     return result;
+}
+
+bool Zipped::ConstIterator::operator==(const Zipped::ConstIterator& second) const {
+    return (first_ == second.first_) || (second_ == second.second_);
+}
+
+bool Zipped::ConstIterator::operator!=(const Zipped::ConstIterator& second) const {
+    return (first_ != second.first_) && (second_ != second.second_);
+}
+
+Zipped::ConstIterator::ConstIterator(Iterator first, Iterator second) : first_(first), second_(second) {
+}
+
+Zipped::Zipped(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end)
+    : begin_(a_begin, b_begin), end_(a_end, b_end) {
+}
+
+Zipped::ConstIterator Zipped::begin() const {
+    return begin_;
+}
+
+Zipped::ConstIterator Zipped::end() const {
+    return end_;
+}
+
+Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end) {
+    return Zipped(a_begin, a_end, b_begin, b_end);
 }
--- zip/zip.h
+++ zip/zip.h
@@ -8,6 +8,35 @@ using Value = std::string;
 using Iterator = std::forward_list<std::string>::const_iterator;
 using ZippedPair = std::pair<const Value&, const Value&>;
 
-using Zipped = std::list<ZippedPair>;
+class Zipped {
+public:
+    class ConstIterator {
+    public:
+        const ZippedPair operator*() const;
+        const ZippedPair operator->() const;
+
+        ConstIterator& operator++();
+        ConstIterator operator++(int);
+
+        bool operator==(const ConstIterator& second) const;
+        bool operator!=(const ConstIterator& second) const;
+
+    private:
+        Iterator first_, second_;
+
+        ConstIterator(Iterator first, Iterator second);
+
+        friend Zipped;
+    };
+
+    Zipped(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end);
+
+    ConstIterator begin() const;
+    ConstIterator end() const;
+
+private:
+    ConstIterator begin_;
+    ConstIterator end_;
+};
 
 Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end);
